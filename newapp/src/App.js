3da3d343ffa699269ef3daec28b86cb10a3ac6c8
Bishal Poudel <poudelb172@gmail.com>
2021-11-04 00:37:41 +0545

    first commit

---
+++ newapp/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Navbar from './Navbar';
+import Newsboxes from './Newsboxes';
+
+function App() {
+  return (
+<div>
+  <Navbar />
+  <Newsboxes />
+  <Newsboxes />
+
+</div>
+ 
+  );
+}
+
+export default App;
---
+++ newapp/src/NewsComp.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import "./Newsbox.css"
+
+export class NewsComp extends Component {
+    static propTypes = {
+
+    }
+
+    render() {
+        return (
+            <>
+                <div className="card-body">
+
+                    <img src="https://source.unsplash.com/user/c_v_r" className="card-img-top" alt="" />
+                    <h5 className="card-title">AOT on early december</h5>
+                    <p className="card-text">Attack on titan will be released on early of january
+                        on the funimations and toonworldIt is one of the best animes released so far.
+                        Levi is coming.</p>
+                    <a href="/" className="btn btn-primary">Read more</a>
+                </div></>
+        )
+    }
+}
+
+export default NewsComp
